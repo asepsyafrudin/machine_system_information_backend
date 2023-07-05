4eef71d17a940537f0b612a0106d7a1d71ce6abb
Asep Syafrudin <jk2141482@Dnia.Asia.Denso>
2023-07-05 10:46:44 +0700

    revisi

--- src/controller/file.js
+++ src/controller/file.js
@@ -1,4 +1,35 @@
-import { deleteFileByIdModels, getFileByDocumentId } from "../models/file.js";
+import {
+  createFilesModels,
+  deleteFileByIdModels,
+  getFileByDocumentId,
+} from "../models/file.js";
+
+export const createFile = async (req, res) => {
+  try {
+    const id = req.body.id;
+    const file = req.files;
+    if (file.length > 0) {
+      for (let index = 0; index < file.length; index++) {
+        let filename =
+          req.protocol +
+          "://" +
+          req.get("host") +
+          "/static/files/" +
+          file[index].filename;
+        await createFilesModels(id, filename, file[index].filename);
+      }
+    }
+    res.status(200).json({
+      msg: "create File Berhasil",
+      data: id,
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "Submit Data Gagal",
+      errMsg: error,
+    });
+  }
+};
 
 export const deleteFileById = async (req, res) => {
   try {
--- src/routes/file.js
+++ src/routes/file.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { deleteFileById, getFile } from "../controller/file.js";
+import { createFile, deleteFileById, getFile } from "../controller/file.js";
+import multer from "multer";
+import { diskStorageFile } from "../config/multer.js";
 
 const router = express.Router();
+const upload = multer({ storage: diskStorageFile });
+
+router.post("/create", upload.array("file", 10), createFile);
 router.delete("/delete/:id", deleteFileById);
 router.get("/getFileByDocumentId/:id", getFile);
 
